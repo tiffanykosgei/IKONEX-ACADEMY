91b2be06165bfcff9b001f4b0295ed2c00735852
Tifany <kosgeitiffany@gmail.com>
2026-06-06 17:01:03 +0300

    Add production CORS

--- ikonexbackend/index.js
+++ ikonexbackend/index.js
@@ -5,7 +5,19 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://ikonex-academy.vercel.app'
+];
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 // Routes (we'll add these one by one)
